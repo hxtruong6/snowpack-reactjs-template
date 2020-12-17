7cab3dbd4393d7b58ce529e5ddcf9dab8f401803
Xuan-Truong Hoang <hxtruong6@gmail.com>
2020-12-18 01:43:03 +0700

    feat: add babel

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,21 +1,29 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 
-
 module.exports = {
   mount: {
-    public: {url: '/', static: true},
-    src: {url: '/dist'},
+    public: { url: '/', static: true },
+    src: { url: '/dist' },
   },
   plugins: [
-    '@snowpack/plugin-react-refresh', 
+    '@snowpack/plugin-react-refresh',
     '@snowpack/plugin-dotenv',
     [
       '@snowpack/plugin-webpack',
       {
-        sourceMap: true
+        sourceMap: true,
       },
     ],
-    '@snowpack/plugin-postcss'
+    '@snowpack/plugin-postcss',
+    [
+      '@snowpack/plugin-babel',
+      {
+        input: ['.js', '.mjs', '.jsx', '.ts', '.tsx'], // (optional) specify files for Babel to transform
+        transformOptions: {
+          // babel transform options
+        },
+      },
+    ],
   ],
   install: [
     /* ... */
