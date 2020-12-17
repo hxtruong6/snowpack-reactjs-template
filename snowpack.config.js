c4a52bd05d207f680c661ba70b40cd7fed65f08d
Xuan-Truong Hoang <hxtruong6@gmail.com>
2020-12-17 15:06:37 +0700

    feat: snowpack custom template

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,10 +1,22 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
+
+
 module.exports = {
   mount: {
     public: {url: '/', static: true},
     src: {url: '/dist'},
   },
-  plugins: ['@snowpack/plugin-react-refresh', '@snowpack/plugin-dotenv'],
+  plugins: [
+    '@snowpack/plugin-react-refresh', 
+    '@snowpack/plugin-dotenv',
+    [
+      '@snowpack/plugin-webpack',
+      {
+        sourceMap: true
+      },
+    ],
+    '@snowpack/plugin-postcss'
+  ],
   install: [
     /* ... */
   ],
